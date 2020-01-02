d14171144a4b079eb257770d5ef15abfd0d02249
Arthur Xavier <arthurxaviergiffoni@gmail.com>
2020-01-02 15:30:05 -0200

    Ajuste factories

--- spec/factories/cars.rb
+++ spec/factories/cars.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
     brand {'HB20 Rspec'}
     manufacture_year {2018}
     model_year {2017}
-    transmission {'Manual'}
+    transmission {:manual}
     gears {5}
     doors {4}
     color {'#ff0000'}
-    seats_option {'Couro'}
+    seats_option {:leather}
     price {100.00}
   end
 end
--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     status {'reserved'}
     price {300.00}
     rent_date {Date.new(2019, 5, 12)}
-    return_date {Date.new(2019, 5, 12)}
+    return_date {Date.new(2019, 5, 20)}
     client
     car
   end
