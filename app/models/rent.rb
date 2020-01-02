7b8153173e19c0895e51cab8fc3c4586f43b5804
Arthur Xavier <arthurxaviergiffoni@gmail.com>
2020-01-02 15:31:51 -0200

    rent date range rules

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,7 +1,7 @@
 class Rent < ApplicationRecord
   extend Enumerize
 
-  before_save :price
+  before_save :set_price, :not_rented_car?
 
   validates :status, :price, :rent_date, :return_date, presence: true
   validate :date_range
@@ -16,9 +16,19 @@ class Rent < ApplicationRecord
     (return_date - rent_date + 1).to_i
   end
 
+  def rented_car?
+    Rent.where(car_id: self.car_id)
+        .where('? > rent_date AND ? < return_date',return_date, return_date)
+        .or(Rent.where(car_id: self.car_id).where('? > rent_date AND ? < return_date',rent_date, rent_date))
+  end
+
+  def not_rented_car?
+    !rented_car?
+  end
+
   private
 
-  def price
+  def set_price
     self.price = car.price * rent_days
   end
 
@@ -26,6 +36,14 @@ class Rent < ApplicationRecord
     return false if !rent_date || !return_date
     if return_date < rent_date
         errors.add(:rent_date, 'return_date cannot be earlier of rent_date')
+    end
+
+    if (return_date - rent_date).to_i > 30
+        errors.add(:return_date, 'date range cannot be most that 30 days')
+    end
+
+    if (return_date - rent_date).to_i < 7
+      errors.add(:return_date, 'date range cannot be less than 7 days')
     end
   end
 end
